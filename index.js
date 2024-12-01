8e8892e7c9e94b825ce7b3314fd25d30f2bf5db9
Dustin Pham <dustinpham07@gmail.com>
2024-12-01 13:29:31 -0800

    uibuild

--- index.js
+++ index.js
@@ -1,65 +1,22 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
 
 const Person = require('./models/person')
 
-
-// Grabbing URL from .env file and connecting to the database
-const url = process.env.MONGODB_URI
-console.log('connecting to', url)
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-
-morgan.token('data', function getData(req) {
-    return JSON.stringify(req.body)
-})
-
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
+app.use(express.static('dist')) // whenever a GET request comes, it will check dist directory and return
 app.use(express.json()) // without this, body property of POST request would be undefined
 app.use(cors()) // allows for frontend to access data from a different PORT
-app.use(express.static('dist')) // whenever a GET request comes, it will check dist directory and return
 
-/* let persons =[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-] */
 
-//let info = `Phonebook has info for ${persons.length} people`
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (request, response) => {
+    console.log("grabbing list of people from database")
     Person.find({}).then(persons => {
         response.json(persons)
     })
@@ -90,7 +47,7 @@ app.post('/api/persons', (request,response) => {
         name: body.name,
         number: body.number
     })
-    var same = persons.find(function(person) {
+/*     var same = persons.find(function(person) {
         return person.name.toString().toLowerCase() === newPerson.name.toString().toLowerCase()
     })
     if (same) {
@@ -98,7 +55,7 @@ app.post('/api/persons', (request,response) => {
         return response.status(400).json({
             error: 'name must me unique'
         })
-    }
+    } */
     newPerson.id = String(Math.floor(Math.random() * 100000))
     newPerson.save().then(savedPerson => {
         response.json(savedPerson)
