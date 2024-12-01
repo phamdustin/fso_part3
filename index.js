7c718c3821a550d46a457c0e0e3b50b02a2fa155
Dustin Pham <dustinpham07@gmail.com>
2024-12-01 15:04:48 -0800

    uibuild

--- index.js
+++ index.js
@@ -32,16 +32,26 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request,response) => {
-    Person.findById(request.params.id).then(person=> {
-        response.json(person)
+    Person.findById(request.params.id)
+    .then(person=> {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        response.status(400).send({error: 'malformatted id'})
     })
 })
 
 app.delete('/api/persons/:id', (request,response) => {
-    const id = request.params.id
-    person = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result=> {
+            response.status(204).end()
+        })
+        .catch(error=> next(error))
 })
 
 app.post('/api/persons', (request,response) => {
@@ -76,6 +86,19 @@ app.post('/api/persons', (request,response) => {
     let timestamp = new Date()
     response.send(info +"<br> <br/>" +timestamp)
 }) */
+
+
+// Error handling middleware has to be placed last!!!
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name==='CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
