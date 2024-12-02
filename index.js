27817b9fd818f8519670fd5330d4bfad710d677b
phamdustin <71998339+phamdustin@users.noreply.github.com>
2024-12-01 17:48:46 -0800

    Merging from development branch, completed up to exercise 3.21 (#3)
    * uibuild
    
    * uibuild
    
    * uibuild
    
    * uibuild

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post('/api/persons', (request,response,next) => {
 
 app.put('/api/persons/:id', (request,response,next) => {
     const {name,number} = request.body
-    Person.findByIdAndUpdate(request.params.id, {name,number}, {new: true})
+    Person.findByIdAndUpdate(request.params.id, {name,number}, {new: true, runValidators:true, context: 'query'})
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -100,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name==='CastError') {
         return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
     next(error)
 }
--- models/person.js
+++ models/person.js
@@ -15,8 +15,20 @@ mongoose.connect(url)
     })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /\d{2}-\d{3}/.test(v)
+            }
+        },
+        required: true
+    }
 })
 
 personSchema.set('toJSON', {
